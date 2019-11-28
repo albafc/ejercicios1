8b08d42c629d1bfab96755693c3ed9875659387c
xp <xp@CETYSDAM00>
2019-11-28 17:51:14 +0100

    con el segundo ejercicio hecho

--- src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
+++ src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
@@ -15,6 +15,20 @@ public class EjerciciosBasicosJava {
             return false;
         }
     }
+    
+      public int multa(int velocidad, boolean birthday) {
+        if (birthday) { //si es mi cumple el guardia me quita 5 de velocidad
+            velocidad -= 5;
+        }
+        if (velocidad <= 60) {
+            return 0;
+        }
+        if (velocidad > 60 && velocidad <= 80) {
+            return 1;
+        }
+        return 2; //el resto de casos, que voy a mas de 80
+    }
+
 
     /**
      * @param args the command line arguments
@@ -24,6 +38,10 @@ public class EjerciciosBasicosJava {
         System.out.println(ejercicios.fiestaArdillas(30, false));
         System.out.println(ejercicios.fiestaArdillas(50, true));
         System.out.println(ejercicios.fiestaArdillas(70, true));
+        ejercicios.multa(30,false);
+        System.out.println(ejercicios.multa(60, false));
+        System.out.println(ejercicios.multa(65, false));
+        System.out.println(ejercicios.multa(90, false));
     }
 
 }
